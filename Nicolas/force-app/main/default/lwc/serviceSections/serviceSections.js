93d430c27d24a0d68a623c4c6339d78b02e21722
vinith <vinithg407@gmail.com>
2026-01-13 22:26:37 -0500

    Created new Section page component

---
+++ Nicolas/force-app/main/default/lwc/serviceSections/serviceSections.js
@@ -0,0 +1,12 @@
+import { LightningElement } from 'lwc';
+
+export default class ServicesSection extends LightningElement {
+    services = [
+        { name: 'Remodeling' },
+        { name: 'Patios' },
+        { name: 'Lawn Service' },
+        { name: 'Trimming' },
+        { name: 'Mulch Installation' },
+        { name: 'And More' }
+    ];
+}

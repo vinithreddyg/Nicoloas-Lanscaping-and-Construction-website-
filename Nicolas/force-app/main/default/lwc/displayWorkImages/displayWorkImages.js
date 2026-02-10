79c352829054e0affe3385b108ddef1ca5622655
vinith <vinithg407@gmail.com>
2026-02-10 16:25:40 -0500

    not fixed

--- Nicolas/force-app/main/default/lwc/displayWorkImages/displayWorkImages.js
+++ Nicolas/force-app/main/default/lwc/displayWorkImages/displayWorkImages.js
@@ -1,9 +1,13 @@
 import { LightningElement } from 'lwc';
-import WORK_IMAGES from '@salesforce/resourceUrl/work_images';
+//import WORK_IMAGES from '@salesforce/resourceUrl/imagesSite';
+import WORK_IMAGES from '@salesforce/resourceUrl/imagesSite';
+import Demo from '@salesforce/resourceUrl/demo1';
 
 export default class DisplayWorkImages extends LightningElement {
-    img1 = WORK_IMAGES + '/img1.png';
-    img2 = `${WORK_IMAGES}/img2.jpg`;
-    img3 = `${WORK_IMAGES}/img3.jpg`;
-    img4 = `${WORK_IMAGES}/img4.jpg`;
+    // Assuming the static resource 'work_images' is a ZIP and images are at the root with .jpg extensions
+    img1 = `${WORK_IMAGES}/images/img1`;
+    img2 = `${WORK_IMAGES}/images/img2`;
+    img3 = `${WORK_IMAGES}/images/img3.jpeg`;
+    img4 = `${WORK_IMAGES}/images/img4.jpeg`;
+    demo = Demo;
 }
